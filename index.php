ad5b3db953ced5a6a779c8f45b2ab16046bebfe4
dooflesdoodles <goertz-niklas@posteo.net>
2019-07-17 23:30:18 +0200

    Added legal-disclosure & privacy-policy

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
-<html lang="de">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<!-- FAVICON-->
-		<link rel="icon" href="img/favicon.ico" type="image/x-icon">
-		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-
-		<!-- TITLE -->
-		<title>Lernen Sie mich näher kennen</title>
-
-		<!-- CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="css/custom.css">
-
-    <!-- JS -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </head>
+  
+    <?php include("header.html");?>
+ 
 
   <body>
     <div class="stars1"></div>
@@ -27,21 +10,7 @@
     <div class="stars4"></div>
     <div class="stars5"></div>
 
-    <nav class="navbar navbar-expand-md">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span>≡</span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <a class="nav-item nav-link" href="#career">Werdegang</a>
-          <a class="nav-item nav-link" href="#jobs">Berufe</a>
-          <a class="nav-item nav-link" href="#skills">Skills</a>
-          <a class="nav-item nav-link" href="#activities">Hobbys</a>
-          <a class="nav-item nav-link" href="#contact">Kontakt</a>
-        </ul>
-      </div>
-    </nav>
+    <?php include("nav.html");?>
 
     <!-- HEADER -->
     <section id="header">
@@ -467,63 +436,10 @@
       </div>
     </section>
 
-    <!-- FOOTER -->
-    <section id="footer">
-      <footer class="page-footer font-small blue pt-4">
-        <div class="container text-center text-md-left">
-          <div class="row">
-            <div class="col-md-6 mt-md-0 mt-3">
-              <h5 class="text-uppercase">#STOP!</h5>
-              <p>Wir sollten reden...<br>
-                Du willst in Kontakt treten oder über ein Projekt reden?
-                <br>
-                dann schreibe mir eine E-Mail an:<br>
-                <a href="#">
-                  <div class="email">
-                    <span>goertz-niklas@posteo.net</span>
-                  </div>
-                </a>
-              </p>
-            </div>
-            <hr class="clearfix w-100 d-md-none pb-3">
-            <div class="col-md-3 mb-md-0 mb-3" >
-              <h5 class="text-uppercase">#KONTAKT</h5>
-              <ul class="list-unstyled">
-                <li>
-                  <a href="https://de.linkedin.com/">
-                    <div class="linkedin"></div>
-                  </a>
-                </li>
-                <li>
-                  <a href="https://github.com/dooflesdoodles/">
-                    <div class="github"></div>
-                  </a>
-                </li>
-                <li>
-                  <a href="tel:+4915221300442">
-                    <div class="whatsapp"></div>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="col-md-3 mb-md-0 mb-3">
-              <h5 class="text-uppercase">#RECHTLICHES</h5>
-              <ul class="list-unstyled">
-                <li>
-                  <a href="#!">Impressum</a>
-                </li>
-                <li>
-                  <a href="#!">Datenschutz</a>
-                </li>
-                <li> 
-                  <p>MIT License - Copyright (c) 2019 Niklas Görtz</p> 
-                </li>
-              </ul>
-            </div>
-          </div>     
-        </div>    
-      </footer>
-    </section>
+
+    <?php include("footer.html");?>
+
+
     <script src="js/custom.js"></script>
   </body>
 </html>
