3db618c1e552def907885fd2eac139d47b678ac4
dooflesdoodles <goertz-niklas@posteo.net>
2019-11-23 01:38:43 +0100

    new codebase for everything

--- js/custom.js
+++ js/custom.js
@@ -1,65 +1,35 @@
-//ASTRONAUT CLICK
+//ASTRONAUT CLICK EVENT
 document.getElementById("astronaut").addEventListener("click", function(){ 
-    astronaut.style.backgroundImage = "url(https://raw.githubusercontent.com/dooflesdoodles/website/master/img/astronaut/idle0_naked.gif)";
+    astronaut.style.backgroundImage = "url(./img/astronaut/idle1.gif)";
 });
 
-//MORE BTN
-function more(){
-    var dots = ["dots1", "dots2", "dots3"];
-    var btns = ["myBtn1", "myBtn2", "myBtn3"];
-    var more = ["more1", "more2", "more3"];
-
-    for(i=0;i<=2;i++){
-        dots[i] = document.getElementById(dots[i]);
-        btns[i] = document.getElementById(btns[i]);
-        more[i] = document.getElementById(more[i]);
-        
-        if(dots[i].style.display === "none"){    
-            dots[i].style.display = "inline";
-            btns[i].innerHTML = "Zeig mir mehr!"; 
-            more[i].style.display = "none";  
-        } 
-        else{
-            dots[i].style.display = "none";
-            btns[i].innerHTML = "weniger anzeigen"; 
-            more[i].style.display = "inline";  
-        } 
-    }
-}
-
-//TO TOP BTN
-$(document).ready(function(){
-	var back_to_top_button = ['<a href="#top" class="back-to-top"></a>'].join("");
-	$("body").append(back_to_top_button)
-	$(".back-to-top").hide();
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) {
-				$('.back-to-top').fadeIn();
-			} else {
-				$('.back-to-top').fadeOut();
-			}
-		});
-		$('.back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 800);
-			return false;
-		});
-	});
+//CONTACT ROCKET MOUSE EVENT
+let rocket = document.getElementById("contactRocket");
+let contactBorder = document.getElementById("contactBorder");
+contactBorder.addEventListener("mouseover", function(){ 
+    rocket.classList.add("rocketWiggle");
+});
+contactBorder.addEventListener("mouseleave", function(){ 
+    rocket.classList.remove("rocketWiggle");
 });
 
-//CATS !!!!
+//CONSOLE
 const args = [
-    `%c  %c  %c  %c  %c  %c  %c  %c  %c    `,
-    'font-size:400px; background-repeat:no-repeat; background-image:url(https://i.giphy.com/media/3ov9jHL2zqPnFPQCA0/giphy.webp);',
-    'font-size:200px; background-repeat:no-repeat; background-image:url(https://i.giphy.com/media/3oEduQAsYcJKQH2XsI/200w.webp);',	
-    'font-size:200px; background-repeat:no-repeat; background-image:url(https://i.giphy.com/media/3o85xoi6nNqJQJ95Qc/200w.webp);',			
-    'font-size:200px; background-repeat:no-repeat; background-image:url(https://i.giphy.com/media/BzEPO5HyaQb6kT8Y18/giphy.webp);',
-    'font-size:250px; background-repeat:no-repeat; background-image:url(https://i.giphy.com/media/VOPK1BqsMEJRS/200w.webp);',
-    'font-size:200px; background-repeat:no-repeat; background-image:url(https://i.giphy.com/media/ktW8FqKodoPUk/200w.webp);',
-    'font-size:200px; background-repeat:no-repeat; background-image:url(https://i.giphy.com/media/Ol2yHMEFJdYEo/200w.webp);',
-    'font-size:200px; background-repeat:no-repeat; background-image:url(https://i.giphy.com/media/cXaeWuJ1oKO4g/giphy.webp);',	
-    'font-size:200px; background-repeat:no-repeat; background-image:url(https://i.giphy.com/media/nNxT5qXR02FOM/giphy.webp);'			
+    `%c  `,
+    'font-size:100px; background-size:100px; background-repeat:no-repeat; background-image:url(https://www.pngarts.com/files/1/Space-Invaders-Alien-Transparent-Background-PNG.png);',				
 ];
 window.console.log.apply(console, args);
+
+//SKILL-IMG BLINK AUTOMATION
+let i = 1;
+function automate(){
+    var filterOn = document.getElementById("skillID-" + i);
+    filterOn.classList.add("skillImgAutomation"); 
+    setTimeout(automate, 440);
+    if(i>=16){
+        i = 1;
+    }else{
+        i++;
+    }
+}
+automate();
